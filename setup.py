a9c217b07c0ad66df54e91828bd5238dd2acd37e
dimd <targolini@gmail.com>
2016-06-29 22:31:38 +0300

    fix setup.py because of wrong classifier

--- setup.py
+++ setup.py
@@ -24,22 +24,27 @@ setup(
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 4 - Beta',
+        'Development Status :: 5 - Production/Stable',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
-        'Topic :: Software Development :: Tools',
+        'Topic :: Software Development :: Embedded Systems'
+        'Topic :: Software Development :: Libraries'
 
         # Pick your license as you wish (should match "license" above)
         'License :: OSI Approved :: MIT License',
 
         # Specify the Python versions you support here. In particular, ensure
         # that you indicate whether you support Python 2, Python 3 or both.
+        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
-        "Programming Language :: Python :: Implementation :: PyPy"
+        "Programming Language :: Python :: Implementation :: PyPy",
+        # OS
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: POSIX :: BSD :: FreeBSD'
     ],
     extras_require=bitnfly_dev
 )
